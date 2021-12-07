0315700ce6329eab233c7cffc524fd35c931efbc
Adinlm <adin.lopez.mun@gmail.com>
2021-12-07 18:37:09 -0300

    Prueba de concepto docx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { Document, Packer, Paragraph, TextRun } from "docx";
+import saveAs from 'file-saver';
+
+//import { experiences, education, skills, achievements } from "./data-ejemplo";
+//import { DocumentCreator } from "./generador-ejemplo"
+import { GeneradorDocs } from "./generador-ejemplo"
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'docx-poc';
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+
+  }
+
+
+  descargarDocumento() {
+
+    //crea un nuevo documento generado por Documentcreator
+    const documentCreator = new GeneradorDocs();
+    const doc = documentCreator.crearParrafos();
+    /*
+    const documentCreator = new DocumentCreator();
+    const doc = documentCreator.create([
+      experiences,
+      education,
+      skills,
+      achievements
+    ]);
+    */
+
+    Packer.toBlob(doc).then(blob => {
+      console.log(blob);
+      saveAs(blob, "ejemplo.docx");
+    });
+  }
+
+  descargarDocumento64() {
+
+    const documentCreator = new GeneradorDocs();
+    const doc = documentCreator.crearParrafos();
+
+
+    Packer.toBase64String(doc).then((string) => {
+      console.log("Soy el string base64: ",string);
+    });
+  }
+
+  
 }
